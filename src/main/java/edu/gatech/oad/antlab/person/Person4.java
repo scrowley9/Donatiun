0fe601a08e99d6d4213e20b2399fdaf1c599596b
tland357 <ikamon357@gmail.com>
2018-09-06 17:15:15 -0400

    Here's my string altering method Person4

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -29,9 +29,14 @@ public class Person4 {
      * @param input the string to be modified
      * @return the modified string
      */
-    private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+    private static String calc(String input) {
+        int[] array = new int[input.length()];
+        char[] charArray = new charArray[input.length()];
+        for (int x = 0; x < input.length(); x++){
+            array[x] = (int) input.charAt(x) + 1;
+            charArray[x] = (char) array[x];
+        }
+        return new String(charArray);
     }
     
     /**
